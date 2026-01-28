1c3fcebd0f9ff634dd8aa3fbf0dbeb4bab35c5c7
Duy Quang Tran (2782) <duyquangtran@kms.com.vn>
2026-01-28 17:03:33 +0700

    Phase 2: config terraform setting

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,20 +1,42 @@
-# # ECS IAM
-# resource "aws_iam_role" "ecs_task_execution" {
-#   name = "ecsTaskExecutionRole"
+# ECS Task Execution Role (pull image, logs)
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.project_name}-ecs-exec"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ecs-tasks.amazonaws.com"
-#       }
-#       Action = "sts:AssumeRole"
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "ecs_exec" {
-#   role       = aws_iam_role.ecs_task_execution.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-# }
+resource "aws_iam_role_policy_attachment" "ecs_exec" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Role (app permissions)
+resource "aws_iam_role" "ecs_task" {
+  name = "${var.project_name}-ecs-task"
+
+  assume_role_policy = aws_iam_role.ecs_task_execution.assume_role_policy
+}
+
+resource "aws_iam_role_policy" "ecs_task_secrets" {
+  role = aws_iam_role.ecs_task.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "secretsmanager:GetSecretValue"
+      ]
+      Resource = aws_secretsmanager_secret.db.arn
+    }]
+  })
+}
