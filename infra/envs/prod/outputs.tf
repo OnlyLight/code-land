e79f74c48286638445c934537a8cbe4585f4824d
Duy Quang Tran (2782) <duyquangtran@kms.com.vn>
2026-02-24 16:59:14 +0700

    Phase 3.2: trigger test pipeline

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.network.private_subnets
 }
+
+# alb dns endpoint
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
