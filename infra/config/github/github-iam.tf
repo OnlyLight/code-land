b6d8457f318fc58d0b11c7f3f4db0af9f97f6b2d
Duy Quang Tran (2782) <duyquangtran@kms.com.vn>
2026-02-26 14:39:57 +0700

    Phase 3.2: config pipeline communicate through OIDC

--- infra/config/github/github-iam.tf
+++ infra/config/github/github-iam.tf
@@ -1,22 +1,39 @@
 data "aws_caller_identity" "current" {}
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
 resource "aws_iam_role" "github_actions" {
-  name = "github-actions-ecr-ecs"
+  name = "github-actions-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:OnlyLight/code-land:*"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:OnlyLight/code-land:*"
+          }
         }
       }
-    }]
+    ]
   })
 }
 
